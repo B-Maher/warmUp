22b191acf448849fac062a947eac23fd54a55a76
B.Maher <bouzid.meher@gmail.com>
2019-10-31 10:27:40 +0100

    Warm up 12

--- warmUp12.js
+++ warmUp12.js
@@ -22,3 +22,53 @@ Technical Details
     -The maximum size of a range will be 100 integers
     -The starting number of a range will be: 0 < n < 100
  */
+
+ //there is a lot of possibilites for exemple for the string '1568141291110137';
+ //The 10 numbers in this string can be:
+ //       15 6 8 14 12 9 11 10 13 7
+ //       15 68 14 
+ //or     1 56 8 14 12 9 11 10 13 7
+ //or     15 68 1 4 12 6 11 10 13 7
+ //so the choise of the 10 number in this exercice is not restricted and is arbitrery
+
+
+//this function will only give us the max and min of an array
+ function searchMaxAndMin(array){
+    var max = array[0];
+    var min = array[0];
+    for (var i = 1; i < array.length; i++){
+        if(array[i] > max){
+            max = array[i];
+        }else if(array[i] < min){
+            min = array[i];
+        }
+    }
+    return [min, max]
+ }
+
+ function mysteryRange(string, n){
+    var result = [];
+    var newString = string;
+    var number = string.length - ((n - (string.length / 2)) * 2)
+    if(string.split('').lastIndexOf('0') > number - 1){
+        newString = string.split('').reverse().join('');
+    }
+    for (var i = 0; i < number; i = i + 2){
+        result.push(newString[1] + newString[0])
+        newString = newString.slice(2)
+    }
+    result = result.concat(newString.split(''));
+    return searchMaxAndMin(result)
+ } 
+
+//this function take the range from 0 to 99 so it is not working for this exercice;
+/* function mysteryRange(string, n, result, i){
+    i = i || 0;
+    result = result || [];
+    if(string.length === n){
+        result = result.concat(string.split(''));
+        return searchMaxAndMin(result)
+    }
+    result[i] = string[0] + string[1];
+    return mysteryRange(string.slice(2), n - 1, result, i + 1)
+ }*/
