67cc72950ace9a94ed48aa6bd03a50e5d4827738
B.Maher <bouzid.meher@gmail.com>
2019-11-05 09:50:09 +0100

    warmup 15

--- warmUp15.js
+++ warmUp15.js
@@ -4,3 +4,16 @@
 //
 //
 // Note : you can`t use any built-in functions .
+function reverseStr(string) {
+	var result = "";
+	var word = string.length - 1;
+	for (var i = string.length - 1; i >= 0; i--){
+		if(string[i] === " " || string[i] === "," || string[i] === "." || i === 0){
+			for(var j = i; j <= word; j++){
+				result += string[j]
+			}
+			word = i;
+		}
+	}
+	return result
+}
