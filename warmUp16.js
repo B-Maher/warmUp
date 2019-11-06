1f14b39b2e89cb70b21af34754b9ea2ec0d32ec6
B.Maher <bouzid.meher@gmail.com>
2019-11-06 09:42:24 +0100

    warmup 16 done

--- warmUp16.js
+++ warmUp16.js
@@ -16,3 +16,26 @@
 // Constraints:
 // 1 <= text.length <= 10^4
 // text consists of lower case English letters only.
+
+function howManyBallon(str){
+	var obj = {};
+	result = 0;
+	for (var i = 0; i < str.length; i++) {
+		if(obj[str[i]] !== undefined){
+			obj[str[i]]++;
+		}else{
+			obj[str[i]] = 1;
+		}
+	}
+	for (var key in obj){
+		if(obj['b'] > 0 && obj['a'] > 0 && obj['n'] > 0 && obj['l'] > 1 && obj['o'] > 1){
+			result++;
+			obj['b']--;
+			obj['a']--;
+			obj['n']--;
+			obj['o'] = obj['o'] - 2;
+			obj['l'] = obj['l'] - 2;
+		}
+	}
+	return result
+}
